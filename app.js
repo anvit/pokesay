414e056af2eb0088b3706d870696b6164d203f45
Anvit Srivastav <anvit1@hotmail.com>
2022-11-14 16:57:11 -0800

    split helper code into utils

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-const textBubble = require('./lib/textBubble');
+const getSpeechBubbleString = require('./lib/getSpeechBubbleString');
 const getPokemonAscii = require('./lib/getPokemonAscii');
 
 const MAX_LINE_LENGTH = 30;
@@ -8,5 +8,5 @@ const input = process.argv[2] || "hello";
 
 const padding = (input.length > MAX_LINE_LENGTH) ? MAX_LINE_LENGTH + DEFAULT_PADDING : (input.length + DEFAULT_PADDING);
 const pokeAsciiArt = getPokemonAscii(padding);
-console.log(textBubble(input, MAX_LINE_LENGTH));
+console.log(getSpeechBubbleString(input, MAX_LINE_LENGTH));
 console.log(pokeAsciiArt);
